4d6d9f6da82427316e075023ce3fb1625546d56b
Gustavo Josias de Lacerda <gustavo@msiautomacao.com.br>
2021-09-20 22:32:03 -0300

    Adicionado modelo

---
+++ src/models/models.py
@@ -0,0 +1,29 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+
+class Usuario(BaseModel):
+    id: Optional[str]=None
+    nome: str
+    telefone: str
+    meus_produtos: List[Produto]
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+    
+class Produto(BaseModel):
+    id: Optional[str]=None
+    usuario: Usuario
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+    
+
+class Pedido(BaseModel):
+    id: Optional[str]=None
+    usuario: Usuario
+    produto: Produto
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str]=None
